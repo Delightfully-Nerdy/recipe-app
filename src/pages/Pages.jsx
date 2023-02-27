f3caacaf95fe564693712eb4e68d4e871bc0b6ca
Dalitso Kasonde <dkasonde11@gmail.com>
2023-02-27 15:36:47 +0200

    feat: add recipe page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,17 @@
-import Home from './Home'
+import Home from './Home';
+import Cusine from './Cusine';
+import {Route, Routes } from "react-router-dom";
+import Searched from './Searched';
+import Recipe from './Recipe';
 
 function Pages() {
   return (
-    <div>
-        <Home />
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+        <Route path="cuisine/:type" element={<Cusine/>} />
+        <Route path='/searched/:search' element={<Searched />} />
+        <Route path='/recipe/:id' element={<Recipe />} />
+    </Routes>
   )
 }
 
